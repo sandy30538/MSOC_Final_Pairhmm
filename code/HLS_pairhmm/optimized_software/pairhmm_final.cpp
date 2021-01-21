59dd4f3d5f78d03c70d1e7e119783bcb62667d76
YYH <D06943001@ntu.edu.tw>
2021-01-21 21:05:06 +0800

    modify

--- code/HLS_pairhmm/optimized_software/pairhmm_final.cpp
+++ code/HLS_pairhmm/optimized_software/pairhmm_final.cpp
@@ -21,12 +21,14 @@ void pairhmm(
     int insert_scores[read_len][haplotype_len] = {0};
     int delete_scores[read_len][haplotype_len] = {0};
     
-    for (int j = 0; j < haplotype_len; ++j) {
+    for (int j = 0; j < haplotype_len; ++j) {        
+		#pragma HLS unroll
         align_scores [0][j] = -pow(2, 15);
         insert_scores[0][j] = -pow(2, 15);
         delete_scores[0][j] = bias_score;
     }
     for (int i = 1; i < read_len; ++i) {
+        #pragma HLS unroll
         align_scores [i][0] = -pow(2, 15);
         insert_scores[i][0] = -pow(2, 15);
         delete_scores[i][0] = -pow(2, 15);
@@ -45,7 +47,6 @@ void pairhmm(
     for (int i=0; i<read_len-1; ++i) {
         for (int j=0; j<haplotype_len-1; ++j) {
             // align scores operation
-			#pragma HLS unroll
             char read_base = read[i];
             char haplotype_base = haplotype[j];
             if (read_BQ[i]=='0') {
